8ab7c17cd8b10229cc139b6aac077adfda221259
apetrova <alevtina.piatrova@gmail.com>
2024-12-09 21:20:26 +0300

    Добавила модификаторы доступа для переменных

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,12 +1,12 @@
 package org.example;
 
 public class Employee {
-    String fullName;
-    String position;
-    String email;
-    String phone;
-    int salary;
-    int age;
+    private String fullName;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
 
     public Employee(String fullName, String position, String email, String phone, int salary, int age) {
         this.fullName = fullName;
