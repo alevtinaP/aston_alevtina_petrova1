43119c7073201038dba6d969ea2efe1279400f88
apetrova <alevtina.piatrova@gmail.com>
2025-01-09 13:46:56 +0300

    Задание выполнено

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         int number = 5;
         int factorial = Factorial.calculateFactorial(number);
-        System.out.println("Факториал числа " + number + " равен " + factorial);
+        System.out.println("Факториал числа" + number + " равен " + factorial);
     }
 }
